4208d5369e533a50c3f3c8a8835471b4e4b9573b
danipadi89 <126537786+danipadi89@users.noreply.github.com>
2023-02-28 00:53:42 -0300

    Add files via upload

---
+++ funciones.js
@@ -0,0 +1,17 @@
+document.getElementById('Futbol').onclick = function(){
+   console.log("apretar aqui");
+     
+   document.getElementById('Futbol').innerHTML = "FUTBOL JUEGO UN 10"; 
+}
+
+document.getElementById('Pato').onclick = function(){
+   console.log("apretar aqui");
+
+    document.getElementById('Pato').innerHTML = "PATO JUEGO UN 1,34"; 
+ }
+
+ document.getElementById('Rugbig').onclick = function(){
+   console.log("apretar aqui");
+   
+    document.getElementById('Rugbig').innerHTML = "RUGBIG JUEGO UN 19,3"; 
+ } 
